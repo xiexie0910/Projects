896aff1578096160b9946ebf2010d1b945443cb9
Alex Xie <alexxienz02@gmail.com>
2025-04-10 20:18:50 +1200

    update files

--- SendEmail&ManageDates/main.py
+++ SendEmail&ManageDates/main.py
@@ -1,6 +1,6 @@
 import smtplib 
  
-my_email = 'alexxienz02@gmail.com'
+my_email = ''
 password = ''
 
 connection = smtplib.SMTP('smtp.gmail.com') 
@@ -9,6 +9,6 @@ connection.starttls() # encrypts the email that we sending
 
 connection.login(user=my_email,password=password)
 
-connection.sendmail(from_addr=my_email,to_addr='zxie089@aucklanduni.ac.nz',msg='Hello my name is alex')
+connection.sendmail(from_addr=my_email,to_addr='',msg='Hello my name is alex')
 
 connection.close()
